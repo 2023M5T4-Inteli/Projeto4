dfe317ae6b6a4c81159307dd45eeddbb730efc96
dmsophia <sophia.dias@sou.inteli.edu.br>
2023-02-28 17:23:57 -0300

    instalação e deploy em truffle
    Primeiro teste de deploy no goerli utilizando a primeira versão do contrato

---
+++ deploy contrato/migrations/1_deployContract.js
@@ -0,0 +1,6 @@
+const contrato = artifacts.require("Seguro");
+
+module.exports = function(deployer) {
+    // deployment steps
+    deployer.deploy(contrato, 10, ["0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"], ["imei1", "imei2"], 15);
+  };
